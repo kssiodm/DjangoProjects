a4034884bfed6a8b3a546e65a2cccb2676af66a8
kssiodm <cassiodemaz@gmail.com>
2024-01-24 17:16:48 -0300

    update

--- ProjetoDjango/settings.py
+++ ProjetoDjango/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
-    BASE_DIR / 'base' / 'static'
+    BASE_DIR / 'base' / 'global' / 'static'
 ]
 
 # Default primary key field type
--- blog/views.py
+++ blog/views.py
@@ -1,25 +1,49 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from blog.data import posts
 
 # Create your views here.
 def blog(resquest):
     print('blog app 3!')
     
     context = {
-        'text': 'Estamos no blog'
+        'title': '- blog',
+        'text': 'Estamos no blog',
+        
     }
     
     return render(
         resquest,
         'blog/index.html',
         context,
+        {
+            'title': 'blog',
+            'posts': posts,
+        }
+    )
+    
+def post(resquest, post_id):
+
+    found_post = None
+    
+    for post in posts:
+        if post('id') == post_id:
+            found_post = post
+            break
+    
+    return render(
+        resquest,
+        'blog/index.html',
+        {
+            'title': 'post',
+            'posts': posts,
+        }
     )
 
 def exemplo(resquest):
     print('exemplo em app 3')
     
     context = {
-        'title': '-blog',
+        'title': '-exemplo',
         'text': 'Estamos no exemplo',
     }
     
